3ca93111bc0cf6bdfae5143d1c93106dd5fff624
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-03-31 22:10:22 +0900

    04 2023/03/31の進捗

--- 04-local-server/src/script.js
+++ 04-local-server/src/script.js
@@ -7,6 +7,13 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+mesh.position.x = 0.7
+mesh.position.y = -0.6
+mesh.position.z = 1
+
+console.log(mesh.position.length())
+
+
 scene.add(mesh)
 
 // Sizes
@@ -19,6 +26,30 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+
+/**
+ * Axes Helper
+ */
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
+
+mesh.scale.x = 2
+mesh.scale.y = 0.25
+mesh.scale.z = 0.5
+
+mesh.rotation.x = Math.PI * 0.25
+mesh.rotation.y = Math.PI * 0.25
+
+
+
+
+
+
+
+
+
+
+
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
