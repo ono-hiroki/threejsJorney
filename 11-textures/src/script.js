ed36ed4a96664b79a4daeb99507faa09547c30fb
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-04-03 10:57:32 +0900

    11 テクスチャの読み込み

--- 11-textures/src/script.js
+++ 11-textures/src/script.js
@@ -4,13 +4,64 @@ import imageSource from '../static/textures/door/color.jpg'
 
 console.log(imageSource)
 
-const image = new Image()
-const texture = new THREE.Texture(image)
-image.addEventListener('load', () =>
+
+const loadingManager = new THREE.LoadingManager()
+loadingManager.onStart = () =>
 {
-    texture.needsUpdate = true
-})
-image.src = '/textures/door/color.jpg'
+    console.log('loading started')
+}
+loadingManager.onLoad = () =>
+{
+    console.log('loading finished')
+}
+loadingManager.onProgress = () =>
+{
+    console.log('loading progressing')
+}
+loadingManager.onError = () =>
+{
+    console.log('loading error')
+}
+
+const textureLoader = new THREE.TextureLoader(loadingManager)
+
+const texture = textureLoader.load(
+    '/textures/door/color.jpg',
+    () =>
+    {
+        console.log('loading finished')
+    },
+    () =>
+    {
+        console.log('loading progressing')
+    },
+    () =>
+    {
+        console.log('loading error')
+    }
+)
+
+const colorTexture = textureLoader.load('/textures/door/color.jpg')
+const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const heightTexture = textureLoader.load('/textures/door/height.jpg')
+const normalTexture = textureLoader.load('/textures/door/normal.jpg')
+const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+
+colorTexture.repeat.x = 2
+colorTexture.repeat.y = 3
+colorTexture.wrapS = THREE.RepeatWrapping
+colorTexture.wrapT = THREE.RepeatWrapping
+colorTexture.wrapS = THREE.MirroredRepeatWrapping
+colorTexture.wrapT = THREE.MirroredRepeatWrapping
+// colorTexture.offset.x = 0.5
+// colorTexture.offset.y = 0.5
+colorTexture.rotation = Math.PI * 0.25
+colorTexture.rotation = Math.PI * 0.25
+colorTexture.center.x = 0.5
+colorTexture.center.y = 0.5
+colorTexture.minFilter = THREE.NearestFilter
 
 /**
  * Base
@@ -25,7 +76,7 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ map: texture })
+const material = new THREE.MeshBasicMaterial({ map: colorTexture })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
