f0649a575043be30a2e14ea36b94ae85a8e6be4b
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-04-03 20:56:12 +0900

    13 3dテキストの表示その2

--- 13-3d-text/src/script.js
+++ 13-3d-text/src/script.js
@@ -2,8 +2,8 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
-import {TextGeometry} from "three/examples/jsm/geometries/TextGeometry";
-
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
+import typefaceFont from 'three/examples/fonts/helvetiker_regular.typeface.json'
 
 /**
  * Base
@@ -21,7 +21,7 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
-
+const matcapTexture = textureLoader.load('textures/matcaps/8.png')
 
 /**
  * Fonts
@@ -32,6 +32,10 @@ fontLoader.load(
     '/fonts/helvetiker_regular.typeface.json',
     (font) =>
     {
+        // Material
+        const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+
+        // Text
         const textGeometry = new TextGeometry(
             'Hello Three.js',
             {
@@ -46,21 +50,29 @@ fontLoader.load(
                 bevelSegments: 5
             }
         )
-        const textMaterial = new THREE.MeshBasicMaterial()
-        const text = new THREE.Mesh(textGeometry, textMaterial)
+        textGeometry.center()
+
+        const text = new THREE.Mesh(textGeometry, material)
         scene.add(text)
+
+        // Donuts
+        const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 32, 64)
+
+        for(let i = 0; i < 100; i++)
+        {
+            const donut = new THREE.Mesh(donutGeometry, material)
+            donut.position.x = (Math.random() - 0.5) * 10
+            donut.position.y = (Math.random() - 0.5) * 10
+            donut.position.z = (Math.random() - 0.5) * 10
+            donut.rotation.x = Math.random() * Math.PI
+            donut.rotation.y = Math.random() * Math.PI
+            const scale = Math.random()
+            donut.scale.set(scale, scale, scale)
+
+            scene.add(donut)
+        }
     }
 )
-
-/**
- * Object
- */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-
-scene.add(cube)
 
 /**
  * Sizes
