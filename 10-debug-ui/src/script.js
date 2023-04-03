2fe111012c8e0832d8b8efce7fe55b54bcf07a3e
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-04-03 10:08:11 +0900

    09 uiデバッグの追加

--- 10-debug-ui/src/script.js
+++ 10-debug-ui/src/script.js
@@ -1,6 +1,16 @@
+import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import gsap from 'gsap'
+import * as dat from 'lil-gui'
+
+const parameters = {
+    color: 0xff0000,
+    spin: () =>
+    {
+        gsap.to(mesh.rotation, { duration: 1, y: mesh.rotation.y + Math.PI * 2 })
+    }
+}
 
 /**
  * Base
@@ -15,7 +25,7 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ color: parameters.color })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
@@ -62,6 +72,34 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI()
+gui.add(mesh.position, 'y')
+gui.add(mesh.position, 'y', - 3, 3, 0.01)
+gui
+    .add(mesh.position, 'y')
+    .min(- 3)
+    .max(3)
+    .step(0.01)
+gui
+    .add(mesh.position, 'y')
+    .min(- 3)
+    .max(3)
+    .step(0.01)
+    .name('elevation')
+gui.add(mesh, 'visible')
+
+gui
+    .addColor(parameters, 'color')
+    .onChange(() =>
+    {
+        material.color.set(parameters.color)
+    })
+
+gui.add(parameters, 'spin')
 
 /**
  * Animate
