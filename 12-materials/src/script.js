f8967c305feb75b239cec1eba7c823a43fd933cb
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-04-03 20:22:27 +0900

    12 メッシュマットキャップ素材

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -48,6 +48,72 @@ const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
 /**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+
+const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
+const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
+const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
+const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+const matcapTexture = textureLoader.load('/textures/matcaps/1.png')
+const gradientTexture = textureLoader.load('/textures/gradients/3.jpg')
+
+/**
+ * Objects
+ */
+// const material = new THREE.MeshBasicMaterial({ map: doorColorTexture })
+// const material = new THREE.MeshBasicMaterial()
+// const material = new THREE.MeshNormalMaterial()
+// const material = new THREE.MeshMatcapMaterial()
+// material.matcap = matcapTexture
+
+// material.color = new THREE.Color('#ff0000')
+// material.color = new THREE.Color('#f00')
+// material.color = new THREE.Color('red')
+// material.color = new THREE.Color('rgb(255, 0, 0)')
+// material.color = new THREE.Color(0xff0000)
+
+// material.map = doorColorTexture
+// material.color = new THREE.Color('#ff0000')
+//
+// material.wireframe = true
+
+// material.transparent = true
+// material.opacity = 0.5
+
+// material.transparent = true
+// material.alphaMap = doorAlphaTexture
+
+// material.flatShading = true
+
+material.side = THREE.DoubleSide
+
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    material
+)
+sphere.position.x = - 1.5
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(1, 1),
+    material
+)
+
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.3, 0.2, 16, 32),
+    material
+)
+torus.position.x = 1.5
+
+scene.add(sphere, plane, torus)
+
+
+
+/**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
@@ -64,6 +130,15 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    sphere.rotation.y = 0.1 * elapsedTime
+    plane.rotation.y = 0.1 * elapsedTime
+    torus.rotation.y = 0.1 * elapsedTime
+
+    sphere.rotation.x = 0.15 * elapsedTime
+    plane.rotation.x = 0.15 * elapsedTime
+    torus.rotation.x = 0.15 * elapsedTime
 
     // Update controls
     controls.update()
