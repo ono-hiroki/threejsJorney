a560ad9ca375bdc6373f2da80010d0e38571d2e9
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-04-04 12:56:39 +0900

    21 apllay force

--- 21-physics/src/script.js
+++ 21-physics/src/script.js
@@ -61,6 +61,7 @@ const sphereBody = new CANNON.Body({
     position: new CANNON.Vec3(0, 4, 0),
     shape: sphereShape,
 })
+sphereBody.applyLocalForce(new CANNON.Vec3(150, 0, 0), new CANNON.Vec3(0, 0, 0))
 world.addBody(sphereBody)
 
 // Floor
@@ -179,6 +180,8 @@ const tick = () =>
     oldElapsedTime = elapsedTime
 
     // Update physics
+    sphereBody.applyForce(new CANNON.Vec3(- 0.5, 0, 0), sphereBody.position)
+
     world.step(1 / 60, deltaTime, 3)
 
     sphere.position.copy(sphereBody.position) // cannonの座標をthreeの座標にコピー
